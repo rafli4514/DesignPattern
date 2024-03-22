9101584e6fd3bdfc76d030ea0837d43492d3cdc3
unknown <muhammadrafli.tkn@gmail.com>
2024-03-22 22:12:53 +0700

    duck tahap 3

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,29 @@
 import java.util.Scanner;
-
 public class Main {
-    public static int tambah(int x, int y){
-        return x + y;
-    }
-
-    public static int kurang(int x, int y){
-        return x - y;
-    }
-
-    public static int kali(int x, int y){
-        return x * y;
-    }
-
-    public static double bagi(int x, int y){
-        if (y != 0){
-            return x / y;
-        }
-        else {
-            return 0;
-        }
-    }
-
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.display();
+        System.out.println("=====================");
         mallard.performFly();
         mallard.performQuack();
+        System.out.println("=====================");
 
         Duck modelDuck = new ModelDuck();
+        modelDuck.display();
+        System.out.println("=====================");
         modelDuck.performQuack();
         modelDuck.performFly();
         modelDuck.setFlyBehavior(new FlyWithRocketPower());
         modelDuck.performFly();
+        System.out.println("=====================");
 
         Duck woodenDuck = new WoodenDuck();
+        woodenDuck.display();
+        System.out.println("=====================");
         woodenDuck.performQuack();
         woodenDuck.setQuackBehavior(new MuteQuack());
         woodenDuck.performQuack();
         woodenDuck.performFly();
-
 
 //        Scanner input = new Scanner(System.in);
 //
@@ -57,18 +41,6 @@ public class Main {
 //
 //        System.out.println("Masukkan angka kedua: ");
 //        int y = input.nextInt();
-
-//        if (menu == 1){
-//            System.out.println(x + " + " + y + " = " + tambah(x, y));
-//        } else if (menu == 2) {
-//            System.out.println(x + " - " + y + " = " + kurang(x, y));
-//        } else if (menu == 3) {
-//            System.out.println(x + " * " + y + " = " + kali(x, y));
-//        } else if (menu == 4) {
-//            System.out.println(x + " / " + y + " = " + bagi(x, y));
-//        } else {
-//            System.out.println("pilihan tidak valid");
-//        }
 
 //        if (menu == 1){
 //            Context Penambahan = new Context(new Tambah());
@@ -91,8 +63,6 @@ public class Main {
 //            System.out.println("pilihan tidak valid");
 //        }
 
-
-
 //        Context Pengurangan = new Context(new Kurang());
 //        System.out.println("213 - 312 = " + Pengurangan.ExecuteHitung(213, 312));
 
@@ -101,8 +71,5 @@ public class Main {
 //
 //        Context Pembagian = new Context(new Bagi());
 //        System.out.println("164 : 6 = " + Pembagian.ExecuteHitung(164, 6));
-
-
-
     }
 }
