003c4b006afadf62b7d40643f92f038b070eae0c
unknown <muhammadrafli.tkn@gmail.com>
2024-03-22 22:27:05 +0700

    analisa

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,77 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Duck mallard = new MallardDuck();
-        mallard.display();
-        System.out.println("=====================");
-        mallard.performFly();
-        mallard.performQuack();
-        System.out.println("=====================");
-
-        Duck modelDuck = new ModelDuck();
-        modelDuck.display();
-        System.out.println("=====================");
-        modelDuck.performQuack();
-        modelDuck.performFly();
-        modelDuck.setFlyBehavior(new FlyWithRocketPower());
-        modelDuck.performFly();
-        System.out.println("=====================");
-
-        Duck woodenDuck = new WoodenDuck();
-        woodenDuck.display();
-        System.out.println("=====================");
-        woodenDuck.performQuack();
-        woodenDuck.setQuackBehavior(new MuteQuack());
-        woodenDuck.performQuack();
-        woodenDuck.performFly();
-
-//        Scanner input = new Scanner(System.in);
+//        Duck mallard = new MallardDuck();
+//        mallard.display();
+//        System.out.println("=====================");
+//        mallard.performFly();
+//        mallard.performQuack();
+//        System.out.println("=====================");
 //
-//        System.out.println("Menu");
-//        System.out.println("1.Penambahan");
-//        System.out.println("2.Pengurangan");
-//        System.out.println("3.Perkalian");
-//        System.out.println("4.Pembagian");
+//        Duck modelDuck = new ModelDuck();
+//        modelDuck.display();
+//        System.out.println("=====================");
+//        modelDuck.performQuack();
+//        modelDuck.performFly();
+//        System.out.println("// Merubah cara terbang");
+//        modelDuck.setFlyBehavior(new FlyWithRocketPower());
+//        modelDuck.performFly();
+//        System.out.println("=====================");
 //
-//        System.out.println("Masukkan Pilihan menu");
-//        int menu = input.nextInt();
-//
-//        System.out.println("Masukkan angka pertama: ");
-//        int x = input.nextInt();
-//
-//        System.out.println("Masukkan angka kedua: ");
-//        int y = input.nextInt();
+//        Duck woodenDuck = new WoodenDuck();
+//        woodenDuck.display();
+//        System.out.println("=====================");
+//        woodenDuck.performQuack();
+//        System.out.println("// Merubah cara bersuara");
+//        woodenDuck.setQuackBehavior(new MuteQuack());
+//        woodenDuck.performQuack();
+//        woodenDuck.performFly();
 
-//        if (menu == 1){
-//            Context Penambahan = new Context(new Tambah());
-//            System.out.print(x + " + " + y + " = ");
-//            System.out.println(Penambahan.ExecuteHitung(x, y));
-//        } else if (menu == 2) {
-//            Context Pengurangan = new Context(new Kurang());
-//            System.out.print(x + " - " + y + " = ");
-//            System.out.println(Pengurangan.ExecuteHitung(x, y));
-//        } else if (menu == 3) {
-//            Context Perkalian = new Context(new Kali());
-//            System.out.print(x + " * " + y + " = ");
-//            System.out.println(Perkalian.ExecuteHitung(x, y));
-//        } else if (menu == 4) {
-//            Context Pembagian = new Context(new Bagi());
-//            System.out.print(x + " / " + y + " = ");
-//
-//            System.out.println(Pembagian.ExecuteHitung(x, y));
-//        } else {
-//            System.out.println("pilihan tidak valid");
-//        }
+        Scanner input = new Scanner(System.in);
 
-//        Context Pengurangan = new Context(new Kurang());
-//        System.out.println("213 - 312 = " + Pengurangan.ExecuteHitung(213, 312));
+        System.out.println("Menu");
+        System.out.println("1.Penambahan");
+        System.out.println("2.Pengurangan");
+        System.out.println("3.Perkalian");
+        System.out.println("4.Pembagian");
 
-//        Context Perkalian = new Context(new Kali());
-//        System.out.println("23 x 41 = " + Perkalian.ExecuteHitung(23, 41));
-//
-//        Context Pembagian = new Context(new Bagi());
-//        System.out.println("164 : 6 = " + Pembagian.ExecuteHitung(164, 6));
+        System.out.println("Masukkan Pilihan menu");
+        int menu = input.nextInt();
+
+        System.out.println("Masukkan angka pertama: ");
+        int x = input.nextInt();
+
+        System.out.println("Masukkan angka kedua: ");
+        int y = input.nextInt();
+
+        if (menu == 1){
+            Context Penambahan = new Context(new Tambah());
+            System.out.print(x + " + " + y + " = ");
+            System.out.println(Penambahan.ExecuteHitung(x, y));
+        } else if (menu == 2) {
+            Context Pengurangan = new Context(new Kurang());
+            System.out.print(x + " - " + y + " = ");
+            System.out.println(Pengurangan.ExecuteHitung(x, y));
+        } else if (menu == 3) {
+            Context Perkalian = new Context(new Kali());
+            System.out.print(x + " * " + y + " = ");
+            System.out.println(Perkalian.ExecuteHitung(x, y));
+        } else if (menu == 4) {
+            Context Pembagian = new Context(new Bagi());
+            System.out.print(x + " / " + y + " = ");
+
+            System.out.println(Pembagian.ExecuteHitung(x, y));
+        } else {
+            System.out.println("pilihan tidak valid");
+        }
+
+        Context Pengurangan = new Context(new Kurang());
+        System.out.println("213 - 312 = " + Pengurangan.ExecuteHitung(213, 312));
+
+        Context Perkalian = new Context(new Kali());
+        System.out.println("23 x 41 = " + Perkalian.ExecuteHitung(23, 41));
+
+        Context Pembagian = new Context(new Bagi());
+        System.out.println("164 : 6 = " + Pembagian.ExecuteHitung(164, 6));
     }
 }
